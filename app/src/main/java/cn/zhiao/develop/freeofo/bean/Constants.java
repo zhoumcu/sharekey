cf23f2e1cb17027e37e32b4089c57b9862e76b35
Administrator <1032324589@qq.com>
2017-04-12 17:53:44 +0800

    fisrt

--- app/src/main/java/cn/zhiao/develop/freeofo/bean/Constants.java
+++ app/src/main/java/cn/zhiao/develop/freeofo/bean/Constants.java
@@ -1,11 +1,11 @@
 package cn.zhiao.develop.freeofo.bean;
 
 public class Constants {
-  public static final String APPID = "1101152570";
+  public static final String APPID = "1106022207";
   public static final String BannerPosID = "9079537218417626401";
   public static final String APPWallPosID = "9007479624379698465";
   public static final String InterteristalPosID = "8575134060152130849";
-  public static final String SplashPosID = "8863364436303842593";
+  public static final String SplashPosID = "9000924169103011";
   public static final String GridAppWallPosID = "9007479624379698465";
   public static final String NativePosID = "5000709048439488";
   public static final String NativeVideoPosID = "2050206699818455";
